536d986e7dc5f45b43506849ece821b632799113
Jessica Fix and Jordan Meier <jbirdmeier@gmail.com>
2016-02-29 10:19:21 -0800

    update routes

--- app/app.php
+++ app/app.php
@@ -28,26 +28,34 @@
         return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
     });
 
+
+
+
+
+
     $app->get("/categories/{id}", function($id) use ($app) {
         $category = Category::find($id);
-        return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks()));
+        return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks(), 'all_tasks' => Task::getAll()));
     });
 
     $app->post("/categories", function() use ($app) {
         $category = new Category($_POST['name']);
         $category->save();
-        return $app['twig']->render('index.html.twig', array('categories' => Category::getAll()));
+        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
     });
 
     $app->post("/tasks", function() use ($app) {
         $description = $_POST['description'];
         $due_date = $_POST['due_date'];
-        $category_id = $_POST['category_id'];
-        $task = new Task($description, $due_date, $id = null, $category_id);
+        $task = new Task($description, $due_date, $id = null);
         $task->save();
-        $category = Category::find($category_id);
-        return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks()));
-});
+        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
+    });
+
+    $app->get("/tasks/{id}", function($id) use ($app) {
+        $task = Task::find($id);
+        return $app['twig']->render('task.html.twig', array('task' => $task, 'categories' => $task->getCategories(), 'all_categories' => Category::getAll()));
+    });
 
     $app->post("/delete_tasks/{id}", function($id) use ($app) {
         $category_id = Category::find($id);
