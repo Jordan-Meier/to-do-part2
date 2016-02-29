598ca91673b8df9e91f97c3d872e706bb5bfe4d1
Jessica Fix and Jordan Meier <jbirdmeier@gmail.com>
2016-02-29 10:42:35 -0800

    create routes for add_tasks and add_categories

--- app/app.php
+++ app/app.php
@@ -28,11 +28,6 @@
         return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
     });
 
-
-
-
-
-
     $app->get("/categories/{id}", function($id) use ($app) {
         $category = Category::find($id);
         return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks(), 'all_tasks' => Task::getAll()));
@@ -67,6 +62,20 @@
        Category::deleteAll();
        return $app['twig']->render('index.html.twig');
    });
+
+   $app->post("/add_tasks", function() use ($app) {
+        $category = Category::find($_POST['category_id']);
+        $task = Task::find($_POST['task_id']);
+        $category->addTask($task);
+        return $app['twig']->render('category.html.twig', array('category' => $category, 'categories' => Category::getAll(), 'tasks' => $category->getTasks(), 'all_tasks' => Task::getAll()));
+    });
+
+    $app->post("/add_categories", function() use ($app) {
+        $category = Category::find($_POST['category_id']);
+        $task = Task::find($_POST['task_id']);
+        $task->addCategory($category);
+        return $app['twig']->render('task.html.twig', array('task' => $task, 'tasks' => Task::getAll(), 'categories' => $task->getCategories(), 'all_categories' => Category::getAll()));
+    });
 
     return $app;
 
